397377dccb627a7a30383323dd3129a8734e3db7
Tomi <tomi.virtanen@centria.fi>
2020-11-05 11:33:07 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 006/Program.cs
+++ Exercises/Part 1/Exercise 006/Program.cs
@@ -7,7 +7,8 @@ namespace exercise_06
         public static void Main(string[] args)
         {
             Console.WriteLine("Give input!");
-            // Write your code here:
+            string message = Console.ReadLine();
+            Console.WriteLine(message);
             
         }
     }
