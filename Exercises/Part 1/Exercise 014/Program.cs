8908ab64c77e71e13b797a8816ac7ab3414e68a0
Tomi <tomi.virtanen@centria.fi>
2020-11-05 14:15:27 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 014/Program.cs
+++ Exercises/Part 1/Exercise 014/Program.cs
@@ -7,7 +7,15 @@ namespace exercise_14
     public static void Main(string[] args)
     {
       Console.WriteLine("Give me the truth!");
-      // Write your code here:
+      
+    string truth = Console.ReadLine();
+
+    bool booleanValue = System.Convert.ToBoolean(truth);
+
+    Console.WriteLine(booleanValue);
+
+    
+
 
     }
   }
