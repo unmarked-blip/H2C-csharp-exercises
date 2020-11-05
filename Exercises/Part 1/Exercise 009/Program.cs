7f765a527bea5acd17a717c4ea43217933beb867
Tomi <tomi.virtanen@centria.fi>
2020-11-05 11:58:19 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 009/Program.cs
+++ Exercises/Part 1/Exercise 009/Program.cs
@@ -6,7 +6,15 @@ namespace exercise_09
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+        Console.Write("Hello, how are you? ");    
+
+        string message1 = Console.ReadLine();
+
+        Console.Write("That's interesting. Tell me more. ");
+
+        string message2 = Console.ReadLine();
+            
+        Console.Write("Thank you for sharing! ");
 
         }
     }
