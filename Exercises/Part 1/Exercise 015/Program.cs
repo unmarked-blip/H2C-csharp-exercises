53ed0b1d5defaee788d58edd6be01816b5c5bddf
Tomi <tomi.virtanen@centria.fi>
2020-11-05 14:36:31 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,33 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+    Console.Write("Give a string: ");
+
+    string words = Console.ReadLine();
+
+    Console.Write("Give a integer: ");
+
+    string integer = Console.ReadLine();
+            
+    int intValue = Convert.ToInt32(integer);
+
+    Console.Write("Give a double: ");
+
+    string userDouble = Console.ReadLine();
+
+    double doubleValue = Convert.ToDouble(userDouble);
+
+    Console.Write("Give a boolean: ");
+
+    string userBoolean = Console.ReadLine();
+
+    bool booleanValue = System.Convert.ToBoolean(userBoolean);
+            
+        
+    Console.WriteLine("Your string: " + words + "\n" + 
+        "Your integer: " + intValue + "\n" + 
+        "Your double: " + doubleValue + "\n" +
+        "Your boolean: " + booleanValue);
 
     }
   }
