733deefce720987cc38a124418c38184abc97719
Tomi <tomi.virtanen@centria.fi>
2020-11-05 11:49:34 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 008/Program.cs
+++ Exercises/Part 1/Exercise 008/Program.cs
@@ -6,8 +6,11 @@ namespace exercise_08
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+         Console.Write("What is your name? ");   
 
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello " + name + "!");
         }
     }
 }
