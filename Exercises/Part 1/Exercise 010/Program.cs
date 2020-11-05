a8e12ba5f20bee1e94d64ef72b231e81645bbc8c
Tomi <tomi.virtanen@centria.fi>
2020-11-05 12:15:52 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -8,8 +8,20 @@ namespace exercise_10
         {
           
 
-           Console.Write(kdfkfj);
+        Console.Write("I will tell a story, but I need some information.\n"
+            + "Give a name for main character: ");
 
+        string name = Console.ReadLine();
+
+        Console.Write("Give the character a profession: ");
+
+        string profession = Console.ReadLine();
+
+        Console.WriteLine("Here is the story:\n" + 
+            "Once upon a time there was a " + profession + " called " + name + ".\n" + 
+            "On her way to work, " + name + " often pondered what being " + profession + " meant to them.\n" + 
+            "When you work as a " + profession + " you meet interesting people.\n"
+            + name + " enjoys their work as " + profession + ", The end.");
 
 
         }
