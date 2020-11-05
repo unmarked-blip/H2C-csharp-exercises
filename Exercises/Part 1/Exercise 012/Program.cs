eef5a37555f0a5f459f5429e42783485815cdf5e
Tomi <tomi.virtanen@centria.fi>
2020-11-05 13:27:21 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 012/Program.cs
+++ Exercises/Part 1/Exercise 012/Program.cs
@@ -6,7 +6,13 @@ namespace exercise_12
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+     Console.WriteLine("Give a number: ");
+
+    string number = Console.ReadLine();
+            
+    int intValue = Convert.ToInt32(number);
+
+    Console.WriteLine("You gave " + intValue);  
 
     }
   }
