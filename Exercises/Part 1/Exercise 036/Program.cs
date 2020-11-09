0993caf21b543af8a88e7d4a3a8df80a14f249c9
Tomi <tomi.virtanen@centria.fi>
2020-11-09 16:24:07 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,7 +6,30 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+
+    while (true) 
+{
+    Console.WriteLine("Give a number");
+
+    int input1 = Convert.ToInt32(Console.ReadLine());
+
+    if (input1 == 0) 
+    {
+        break;
+    }
+
+    else if (input1 < 0)
+    {
+    Console.WriteLine("That is negative.");
+    }
+
+    else if (input1 > 0)
+    {
+    int power = Convert.ToInt32(input1 * input1);
+    Console.WriteLine(power);
+    }
+    
+}
 
     }
   }
