d6ffd32c92e2f1321606f6dc27f3f55e44c04a98
Tomi <tomi.virtanen@centria.fi>
2020-11-09 11:22:47 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -7,7 +7,30 @@ namespace exercise_23
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+    Console.Write("Give first number: ");
+
+    string firstNumber = Console.ReadLine();
+
+    int firstValue = Convert.ToInt32(firstNumber); 
+
+    Console.Write("Give second number: ");
+
+    string secondNumber = Console.ReadLine();
+
+    int secondValue = Convert.ToInt32(secondNumber);
+
+    int sum = firstValue + secondValue;
+
+    int deduct = firstValue - secondValue;
+
+    int multiply = firstValue * secondValue;
+
+    double divide = (double)firstValue / (double)secondValue;
+
+    Console.WriteLine((double)firstValue + " + " + secondValue + " = " + sum + "\n" +
+        firstValue + " - " + secondValue + " = " + deduct + "\n" +
+        firstValue + " * " + secondValue + " = " + multiply + "\n" +
+        firstValue + " / " + secondValue + " = " + divide);
 
     }
   }
