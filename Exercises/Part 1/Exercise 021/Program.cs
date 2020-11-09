e9b732fd097a7a83616973be1611e4cf7ee7bdb0
Tomi <tomi.virtanen@centria.fi>
2020-11-09 10:42:18 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -7,7 +7,21 @@ namespace exercise_21
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+    Console.Write("Give first number: ");
+
+    string firstNumber = Console.ReadLine();
+
+    int firstValue = Convert.ToInt32(firstNumber); 
+
+    Console.Write("Give second number: ");
+
+    string secondNumber = Console.ReadLine();
+
+    int secondValue = Convert.ToInt32(secondNumber); 
+
+    double average = (firstValue + secondValue) / 2.0 ;
+
+    Console.WriteLine("The average for the numbers is " + average);
 
     }
   }
