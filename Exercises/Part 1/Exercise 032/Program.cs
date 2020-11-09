dddedf841fc0859e7fbd6598771a6234b9117374
Tomi <tomi.virtanen@centria.fi>
2020-11-09 14:16:31 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 032/Program.cs
+++ Exercises/Part 1/Exercise 032/Program.cs
@@ -6,7 +6,21 @@ namespace exercise_32
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+
+    Console.Write("Speak, friend, and enter: ");
+
+    string friend = Console.ReadLine();
+
+    string friend2 = "Mellon";
+
+        if (string.Compare(friend, friend2) == 0) 
+{
+        Console.WriteLine("Welcome, friend.");
+}
+        else
+{
+        Console.WriteLine("They've got a cave troll!");
+}
 
     }
   }
