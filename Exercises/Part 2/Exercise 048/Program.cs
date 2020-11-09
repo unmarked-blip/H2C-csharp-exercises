4fad14ad3767f485a0655a4f550e340037c0386c
Tomi <tomi.virtanen@centria.fi>
2020-11-09 19:07:15 +0200

    9.11.2020

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -7,8 +7,44 @@ namespace exercise_48
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+    int sum = 0;
+    int amount = 0;
+    int evenNumbers = 0;
+    int oddNumbers = 0;
  
+    Console.WriteLine("Give numbers: ");
+
+while (true) 
+{
+    int input1 = Convert.ToInt32(Console.ReadLine());
+
+    if (input1 == -1) 
+    {
+        double mean = ((double)sum / (double)amount);
+        Console.WriteLine("Thx! bye! \n" + 
+        "Total sum of numbers: " + sum + "\n" +
+        "Numbers: " + amount + "\n" + 
+        "Average: " + mean + "\n" +
+        "Even: " + evenNumbers + "\n" +
+        "Odd: " + oddNumbers);
+        break;
+    }
+
+    else
+    {
+        sum = (sum + input1);
+        amount = (amount + 1);
+                    if (input1%2 == 0) 
+                    {
+                        evenNumbers = (evenNumbers + 1);
+                    }
+                    else
+                    {
+                        oddNumbers = (oddNumbers + 1);
+                    }
+    }
+ 
+}
     }
   }
 }
