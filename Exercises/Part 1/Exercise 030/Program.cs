80fc4e72c7597e3b9d73be9166dad06c80f63d18
Tomi <tomi.virtanen@centria.fi>
2020-11-09 13:28:10 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,8 +6,52 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
 
+     Console.Write("Give your percent [0 - 100]: ");
+
+    string firstNumber = Console.ReadLine();
+
+    int firstValue = Convert.ToInt32(firstNumber);
+
+        if (firstValue < 0) 
+{
+        Console.WriteLine("Impossible!");
+}
+
+        else if (firstValue <= 49)
+{
+        Console.WriteLine("Fail");
+}
+
+        else if (firstValue <= 59)
+{
+        Console.WriteLine("1");
+}
+
+        else if (firstValue <= 69)
+{
+        Console.WriteLine("2");
+}
+
+        else if (firstValue <= 79)
+{
+        Console.WriteLine("3");
+}
+
+        else if (firstValue <= 89)
+{
+        Console.WriteLine("4");
+}
+
+        else if (firstValue <= 100)
+{
+        Console.WriteLine("5");
+}
+
+        else
+{
+        Console.WriteLine("Outstanding!");
+}
     }
   }
 }
