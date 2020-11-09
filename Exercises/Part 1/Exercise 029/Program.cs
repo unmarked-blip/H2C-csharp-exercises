dbffdd1bd41b70cbdc99704763b41383d465675e
Tomi <tomi.virtanen@centria.fi>
2020-11-09 13:17:29 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,7 +6,34 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+
+     Console.Write("Give the first number: ");
+
+    string firstNumber = Console.ReadLine();
+
+    int firstValue = Convert.ToInt32(firstNumber);
+
+    Console.Write("Give the second number: ");
+
+    string secondNumber = Console.ReadLine();
+
+    int secondValue = Convert.ToInt32(secondNumber);
+
+        if (firstValue == secondValue) 
+{
+        Console.WriteLine("They are equal!");
+}
+
+        else if (firstValue > secondValue)
+{
+        Console.WriteLine("The larger number is " + firstValue);
+}
+
+
+        else
+{
+        Console.WriteLine("The larger number is " + secondValue);
+}
 
     }
   }
