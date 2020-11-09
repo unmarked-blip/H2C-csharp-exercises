a08e1b574abba6dd909b7f67b17eca8ba271fd12
Tomi <tomi.virtanen@centria.fi>
2020-11-09 16:47:03 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 039/Program.cs
+++ Exercises/Part 1/Exercise 039/Program.cs
@@ -6,7 +6,29 @@ namespace exercise_39
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+ 
+    int sum = 0;
+ 
+while (true) 
+{
+    Console.WriteLine("Give a number");
+
+    int input1 = Convert.ToInt32(Console.ReadLine());
+
+    if (input1 == 0) 
+    {
+        Console.WriteLine("Total sum of numbers: " + sum);
+        break;
+    }
+
+    else
+    {
+        sum = (sum + input1);            
+    }
+   
+    
+}
+
     }
   }
 }
