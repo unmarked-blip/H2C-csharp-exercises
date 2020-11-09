85bd2b46e204faa4fd19a78e6a82aed420e25793
Tomi <tomi.virtanen@centria.fi>
2020-11-09 14:52:24 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,7 +6,23 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+
+    Console.Write("Give the first string: ");
+
+    string firstWord = Console.ReadLine();
+
+    Console.Write("Give the second string: ");
+
+    string secondWord = Console.ReadLine();
+
+        if (string.Compare(firstWord, secondWord) == 0) 
+{
+        Console.WriteLine("Echo!");
+}
+        else
+{
+        Console.WriteLine("Nope!");
+}
 
     }
   }
