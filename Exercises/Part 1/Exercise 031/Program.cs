b126f3bcad2a34e5be74881a6d6897248dbfe165
Tomi <tomi.virtanen@centria.fi>
2020-11-09 13:44:55 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -6,7 +6,21 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+   
+    Console.Write("Give a number: ");
+
+    string firstNumber = Console.ReadLine();
+
+    int firstValue = Convert.ToInt32(firstNumber);
+
+        if (firstValue%2 == 0) 
+{
+        Console.WriteLine("It is even.");
+}
+        else
+{
+        Console.WriteLine("It is odd.");
+}
 
     }
   }
