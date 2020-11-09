92b331167c94d63b853032155f5852bb22f42dc0
Tomi <tomi.virtanen@centria.fi>
2020-11-09 16:37:58 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,30 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+
+    int sum = 0;
+ 
+while (true) 
+{
+    Console.WriteLine("Give a number");
+
+    int input1 = Convert.ToInt32(Console.ReadLine());
+
+    if (input1 == 0) 
+    {
+        Console.WriteLine("Total amount of negative numbers: " + sum);
+        break;
+    }
+
+    else if (input1 < 0)
+    {
+        sum = (sum + 1);
+    }
+
+   
+    
+}
+
 
     }
   }
