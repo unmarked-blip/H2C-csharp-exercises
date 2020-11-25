00031f507c663dd84e7ed110155079eb804e46ef
Tomi <tomi.virtanen@centria.fi>
2020-11-25 15:13:55 +0200

    25.11.2020

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -6,13 +6,26 @@ namespace exercise_50
   {
     public static void Main(string[] args)
     {
-      // Call your method here:
- 
+        
+    Console.WriteLine("How many times?");
+
+    int input1 = Convert.ToInt32(Console.ReadLine());
+
+    int loopCount = 1;
+  
+  while (loopCount <= input1)
+    {
+    PrintPhrase();
+    loopCount = (loopCount + 1);
     }
 
-    // Write your method here:
-    public static void PrintPhrase() {
+    }
+    
 
+    // Write your method here:
+    public static void PrintPhrase() 
+    {
+        Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
