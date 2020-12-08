6eb96b8b2c8169e03671e1ab7dd727ac607bd2d6
Tomi <tomi.virtanen@centria.fi>
2020-12-08 15:58:56 +0200

    8.12.2020

--- Exercises/Part 2/Exercise 055/Program.cs
+++ Exercises/Part 2/Exercise 055/Program.cs
@@ -6,10 +6,16 @@ namespace exercise_55
   {
     public static void Main(String[] args)
     {
+        int number1 = NumberUno();
 
+        Console.WriteLine("The number is: " + number1);
     }
 
     // Write your method here:
+    public static int NumberUno()
+    {
+        return 1;
+    }
 
   }
 }
