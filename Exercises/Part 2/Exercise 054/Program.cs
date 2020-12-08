b642fc7239585c3b7616efb72dbbbe8d7cf2d349
Tomi <tomi.virtanen@centria.fi>
2020-12-08 15:32:03 +0200

    8.12.2020

--- Exercises/Part 2/Exercise 054/Program.cs
+++ Exercises/Part 2/Exercise 054/Program.cs
@@ -4,13 +4,41 @@ namespace exercise_54
 {
   class Program
   {
-    public static void Main(String[] args)
+     public static void Main(String[] args)
     {
-      // Call your method here:
+        Console.WriteLine("Give the smallest number of the range: ");
+
+        int input1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Give the biggest number of the range: ");
+
+        int input2 = Convert.ToInt32(Console.ReadLine());
+
+        DivisibleByThreeInRange(input1,input2);
 
     }
 
-    // Write your method here:
+    public static void DivisibleByThreeInRange(int input1, int input2) 
+    {
+                        
+        while (input1 <= input2)  
+            {   
+               if ((input1 % 3) == 0)
+                  {
+                    Console.WriteLine(input1);
+                  }
+               input1++;
+            }
+    
+        
+    
+    }
+                
+            
 
+            
+            
   }
+
 }
+
