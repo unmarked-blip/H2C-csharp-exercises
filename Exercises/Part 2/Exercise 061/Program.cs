a9071d5562ae2c5c62aa842421745d78ee53a567
Tomi <tomi.virtanen@centria.fi>
2020-12-15 15:11:11 +0200

    15.12.2020

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -6,27 +6,62 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
+        Console.WriteLine("Give a number");
+
+        int input1 = Convert.ToInt32(Console.ReadLine());
+            
+        ChristmasTree(input1);
 
     }
 
-    public static void PrintStars(int number)
-    {
 
+
+
+    public static void PrintStars(int input1)
+    {
+         for (int number = 0; number<input1; number++)
+                {
+                    Console.Write("*");
+                }
+      Console.WriteLine("");
+         
     }
 
-    public static void PrintSpaces(int number)
+    public static void PrintSpaces(int input1)
     {
-
+         for (int number = 0; number<input1; number++)
+                {
+                    Console.Write(" ");
+                }
     }
 
-    public static void PrintRightTriangle(int size)
+    public static void PrintRightTriangle(int input1)
     {
-
+        for (int i=1; i<=input1; i++)
+			{
+                PrintSpaces(input1-i);
+                PrintStars(i);
+			}
     }
 
-    public static void ChristmasTree(int height)
+    public static void ChristmasTree(int input1)
     {
+        for (int i=0; i<=input1; i++)
+                {
+                  PrintSpaces(input1-i);
+                  PrintStars((i*2)-1);
+                  
+                }
+        
+        int lastRow = ((input1*2)-3)/2;
 
+        for (int i = 0; i < 2; i++)
+            {
+                PrintSpaces(lastRow);
+                PrintStars(3);
+
+            }   
+        
     }
   }
 }
