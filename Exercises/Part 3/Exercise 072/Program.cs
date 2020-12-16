cfa0a6b39dcb55224003ae842f12712127ab84cc
Tomi <tomi.virtanen@centria.fi>
2020-12-16 14:00:09 +0200

    16.12.2020

--- Exercises/Part 3/Exercise 072/Program.cs
+++ Exercises/Part 3/Exercise 072/Program.cs
@@ -7,6 +7,45 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
+      List<int> list = new List<int>();
+      while (true)
+      {
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input == 9999)
+        {
+          break;
+        }
+        list.Add(input);
+      }
+
+      int smallest = list[0];
+      int index = list.IndexOf(smallest);
+
+        for(int i = 0; i < list.Count; i++) 
+        {
+        
+            int number = list[i];
+        
+            if (smallest > number) 
+                {
+                    smallest = number;
+                }
+                       
+        }
+        
+            Console.WriteLine("The smallest number: " + smallest);
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            int number = list[i];
+                        
+                if (smallest == number)
+                {
+                    Console.WriteLine("The smallest number is at index: " + i);
+                }
+        }
+
+        
 
     }
   }
